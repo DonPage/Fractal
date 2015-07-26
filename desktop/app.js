7b9fbc2774926c84bc39f40183cf91100d86296f
DonPage <don.page.work@gmail.com>
2015-07-25 22:29:05 -0400

    ADDED: parsing data.

--- desktop/app.js
+++ desktop/app.js
@@ -11,10 +11,24 @@ var robot = require('robotjs');
 
 io.on('connection', function(socket){
 
-    socket.on('event', function (data) {
-        console.log("IOevent:", data);
+    socket.on('leftclick', function (data) {
+        console.log("leftclick:", data);
+        robot.mouseClick();
+    });
+
+
+
+    socket.on('movemouse', function (data) {
+        var parse = JSON.parse(data);
+
+        //var mouse = robot.getMousePos();
+
+        console.log(parse);
 
     })
+
+
+
 
 });
 server.listen(3000);
@@ -24,13 +38,13 @@ server.listen(3000);
  * Testing robot stuff
  */
 
-var mouse = robot.getMousePos();
-console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
-
-for (var i = 0; i < 100; i++) {
-    robot.moveMouse(mouse.x+i, mouse.y+i);
-    if (i == 99) {
-        mouse = robot.getMousePos();
-        console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
-    }
-}
+//var mouse = robot.getMousePos();
+//console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+//
+//for (var i = 0; i < 100; i++) {
+//    robot.moveMouse(mouse.x+i, mouse.y+i);
+//    if (i == 99) {
+//        mouse = robot.getMousePos();
+//        console.log("Mouse is at x:" + mouse.x + " y:" + mouse.y);
+//    }
+//}
